7273e390bc08c706c944937bedd1096a186e3ce2
detria <detrianoelllucan@gmail.com>
2022-06-13 18:46:09 +0200

    Validaciones registro funcionando

--- FRONTEND/src/app/components/register/register.component.ts
+++ FRONTEND/src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import {FormsModule, NgForm} from '@angular/forms'
+import { AbstractControl, Form, FormBuilder, FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms'
 import Swal from 'sweetalert2'
 import { Router } from '@angular/router';
 
@@ -11,29 +11,38 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(public userService:UserService,private router:Router) { }
+  constructor(public userService: UserService, private router: Router) {
 
-  ngOnInit(): void {
-    
   }
 
-  registerUser(form:NgForm){
-    this.userService.createUser(form.value).subscribe(
-      res=>{
+  ngOnInit(): void {
+
+  }
+
+  userForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required,Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  registerUser() {
+    this.userService.createUser(this.userForm.value).subscribe(
+      res => {
         this.userService.getUsers()
       },
-      err=>console.log(err)
+      err => console.log(err)
     )
     Swal.fire({
       title: 'User registered!',
       text: '',
       icon: 'success',
       confirmButtonText: 'OK'
-    }).then(() =>{
-      form.reset();
+    }).then(() => {
+      this.userForm.reset();
       this.router.navigate(['/login'])
     })
-    
   }
+
 
 }
